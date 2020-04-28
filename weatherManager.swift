cb69675f5230895c11eee24b7dc6149d9361296a
Maksim Pansevich <maxrepan@gmail.com>
2020-04-28 14:45:42 +0300

    Getting data from API

--- weatherManager.swift
+++ weatherManager.swift
@@ -9,10 +9,32 @@
 import Foundation
 
 struct WeatherManager {
-    let url = "http://api.openweathermap.org/data/2.5/weather?appid=ca86c5d774a8764190b560bac9b11a25&units=metric"
+    let url = "https://api.openweathermap.org/data/2.5/weather?appid=ca86c5d774a8764190b560bac9b11a25&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(url)&q=\(cityName)"
         print(urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            
+        }
     }
 }
